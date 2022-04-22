ac45fe3638a19a3d30668f3e53eee17f6c986923
Tusshar-developer <tussharspinner@gmail.com>
2022-04-22 22:11:28 +1000

    UI update and code cleaning.

--- script.js
+++ script.js
@@ -1,158 +1,102 @@
-let data = [];
-
 var threshold = 0.01;
 var inputTime;
 let integerSign = true;
 
 let questionNum = 0;
 
-document.addEventListener('keypress', (e) => {
+document.addEventListener("keypress", (e) => {
+  var keys = [
+    ["&", "1"],
+    ["é", "2"],
+    ['"', "3"],
+    ["'", "4"],
+    ["(", "5"],
+    ["§", "6"],
+    ["è", "7"],
+    ["!", "8"],
+    ["ç", "9"],
+    ["à", "0"],
+  ];
 
-  switch(e.key) {
-    case '&':
-      case '1':
-        inputTime = threshold * 1;
-        executeOrder66()
-        break;
-        
-    case 'é':
-      case '2':
-        inputTime = threshold * 2;
-        executeOrder66()
-        break;
-        
-    case '\"':
-      case '3':
-        inputTime = threshold * 3;
-      executeOrder66();
+  for (let i = 0; i < keys.length; i++) {
+    if (keys[i].includes(e.key)) {
+      if (keys[i][1] == "0") {
+        keyPress(10);
+      } else {
+        keyPress(keys[i][1]);
+      }
+
       break;
-      
-      case '\'':
-        case '4':
-          inputTime = threshold * 4;
-          executeOrder66();
-          break;
-          
-          case '\(':
-            case '5':
-      inputTime = threshold * 5;
-      executeOrder66();
-      break;
-      
-      case '§':
-        case '6':
-          inputTime = threshold * 6;
-          executeOrder66();
-      break;
-      
-      case 'è':
-        case '7':
-          inputTime = threshold * 7;
-          executeOrder66();
-          break;
-          
-          case '!':
-            case '8':
-              inputTime = threshold * 8;
-              executeOrder66();
-              break;
-              
-              
-        case 'ç':
-          case '9':
-            inputTime = threshold * 9;
-            executeOrder66();
-            break;
-            
-            case 'à':
-              case '0':
-                inputTime = threshold * 10;
-                executeOrder66();
-                break;
-                
-              }
-            })
-            
+    }
+  }
+});
+
 function executeOrder66() {
-  
-  document.body.classList.remove('active');
+  document.querySelector(".timesUpMsg").classList.remove("active");
   questionNum++;
-  let x = 0;
   let isPaused = false;
-  var hasReachedZéro = false;
-  
-  
+  var hasReachedZero = false;
+
   let inputMinutes = inputTime;
   let timeSecond = (inputMinutes * 60).toFixed(1);
   let startingVal = timeSecond;
-  
-  const timeH = document.getElementById("timeI");
-  
+
+  const timerDOM = document.querySelector(".timer");
+
   displayTime(timeSecond);
-  
+
   const countDown = setInterval(() => {
-    if (!isPaused) {
-      var zeroReplacer;
+    if (isPaused) return;
 
-      for (let i = 0; i<10000; i++) {
-        var ans = startingVal - i;
-        if (ans < 0) {
-          zeroReplacer = startingVal - (i - 1);
-          console.log(zeroReplacer);
-          break;
-        }
-      }
-      
-      if (timeSecond == zeroReplacer) {
-        endCount();
-        // countUp();
-        hasReachedZéro = true;
-        integerSign = false;
-        // clearInterval(countDown);
-      }
-      
-      if (x !== 0) {
-        const hr  = Math.floor(timeSecond / 3600);
-        const min = Math.floor((timeSecond / 60) % 60);
-        const sec = Math.floor(timeSecond % 60);
-        let str = String(hr).padStart(2, "0") + ":" + String(min).padStart(2, "0") + ":" + String(sec).padStart(2, "0");
-        
-        data.push(str);
-        
-        return
-      };
-      
-      if (!isPaused && !hasReachedZéro) timeSecond--;
-      if (!isPaused && hasReachedZéro ) {
-        timeSecond++
-      }
-      
-      displayTime(timeSecond);
+    var zeroReplacer;
 
+    for (let i = 0; i < 10000; i++) {
+      var ans = startingVal - i;
+      if (ans < 0) {
+        zeroReplacer = startingVal - (i - 1);
+        break;
+      }
     }
+
+    if (timeSecond == zeroReplacer) {
+      document.querySelector(".timesUpMsg").classList.add("active");
+      hasReachedZero = true;
+      integerSign = false;
+    }
+
+    hasReachedZero ? timeSecond++ : timeSecond--;
+
+    displayTime(timeSecond);
   }, 1000);
-  
-  document.addEventListener('keypress', (e) => {
-    if (e.code == "Space") isPaused ? isPaused = false : isPaused = true;
+
+  document.addEventListener("keypress", (e) => {
+    if (e.code == "Space") pauseFunc();
 
     if (e.key === "Enter") {
+      let inverseOfTimeSecond = 0 - timeSecond;
 
-      let inverseOfTimeSecond = 0 -  timeSecond;
-
-      const hr  = Math.floor(timeSecond / 3600);
+      const hr = Math.floor(timeSecond / 3600);
       const min = Math.floor((timeSecond / 60) % 60);
       const sec = Math.floor(timeSecond % 60);
-      let str = String(hr).padStart(2, "0") + ":" + String(min).padStart(2, "0") + ":" + String(sec).padStart(2, "0");
-      
+      let str =
+        String(hr).padStart(2, "0") +
+        ":" +
+        String(min).padStart(2, "0") +
+        ":" +
+        String(sec).padStart(2, "0");
 
-      !document.body.classList.contains('active') ? 
-      localStorage.setItem(`Question Number: ${questionNum}`, `${timeSecond} seconds, ${str}`):
-      localStorage.setItem(`Question Number: ${questionNum}`, `${inverseOfTimeSecond} seconds, ${str}`);
-
-      x++;
+      !document.body.classList.contains("active")
+        ? localStorage.setItem(
+            `Question Number: ${questionNum}`,
+            `${timeSecond} seconds, ${str}`
+          )
+        : localStorage.setItem(
+            `Question Number: ${questionNum}`,
+            `${inverseOfTimeSecond} seconds, ${str}`
+          );
 
       reset();
-    };
+    }
 
     if (e.key === "t") {
       var values = [],
@@ -160,55 +104,51 @@ function executeOrder66() {
         i = keys.length;
 
       while (i--) {
-          values.push( localStorage.getItem(keys[i]) );
+        values.push(localStorage.getItem(keys[i]));
       }
 
       var seconds = 0;
       for (let j = 0; j < values.length; j++) {
         var [hrs, mins, secs] = values[j].split(":");
 
-        seconds += secs + (mins * 60) + (hrs * 3600); 
+        seconds += secs + mins * 60 + hrs * 3600;
       }
 
-      var timeSaved = String(Math.floor(seconds / 3600)).padStart(2, "0") + ":" + String(Math.floor((seconds / 60) % 60)).padStart(2, "0") + ":" + String(Math.floor(seconds % 60)).padStart(2, "0");
+      var timeSaved =
+        String(Math.floor(seconds / 3600)).padStart(2, "0") +
+        ":" +
+        String(Math.floor((seconds / 60) % 60)).padStart(2, "0") +
+        ":" +
+        String(Math.floor(seconds % 60)).padStart(2, "0");
 
       console.log(timeSaved);
-    };
+    }
 
     if (e.key === "c") {
       window.localStorage.clear();
     }
   });
 
-  function countUp() {
-    return setInterval(() => {
-      if (x !== 0) {
-        return;
-      }
-      timeSecond++;
-      displayTime(timeSecond);
-    }, 1000);
-  }
-  
   function displayTime(second) {
-    const hr  = Math.floor(second / 3600);
+    const hr = Math.floor(second / 3600);
     const min = Math.floor((second / 60) % 60);
     const sec = Math.floor(second % 60);
-    let str = String(hr).padStart(2, "0") + ":" + String(min).padStart(2, "0") + ":" + String(sec).padStart(2, "0")
-    timeH.innerHTML = str;
-  }
-  
-  function endCount() {
-    document.body.classList.toggle('active');
+    let str =
+      String(hr).padStart(2, "0") +
+      ":" +
+      String(min).padStart(2, "0") +
+      ":" +
+      String(sec).padStart(2, "0");
+    timerDOM.innerHTML = str;
   }
 
   function reset() {
     var resetVal = "00:00:00";
-    document.querySelector('#timeI').textContent = resetVal;
-    document.body.classList.remove('active');
+    document.querySelector(".timer").textContent = resetVal;
+    document.querySelector(".timesUpMsg").classList.remove("active");
     clearInterval(countDown);
   }
-};
+}
 
 function keyPress(keyNum) {
   inputTime = threshold * keyNum;
